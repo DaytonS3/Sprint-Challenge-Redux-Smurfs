f72a7c8a20f38ba2f2737d75c5b81dc96dd8ce67
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-12 11:19:57 -0400

    Setup Actions / reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,18 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
+
+export const fetch = () => {
+  return dispatch => {
+    dispatch({type: FETCHING})
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}
